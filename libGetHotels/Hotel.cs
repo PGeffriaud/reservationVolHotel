549e2f25e64b3617c20c1931a3c42370ba870fd3
TidyMaze <GrosKiff@gmail.com>
2016-01-28 09:27:45 +0100

    libGetHotels

---
+++ libGetHotels/Hotel.cs
@@ -0,0 +1,20 @@
+﻿namespace booking.libGetHotels
+{
+    public class Hotel
+    {
+        public int id;
+        public string name;
+        public string city;
+        public string adress;
+        public int price;
+
+        public Hotel(int id, string name, string city, string adress, int price)
+        {
+            this.id = id;
+            this.name = name;
+            this.city = city;
+            this.adress = adress;
+            this.price = price;
+        }
+    }
+}
