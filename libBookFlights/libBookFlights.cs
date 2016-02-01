0fd8c5f38b86c83b419860e0502f82cd56bc7965
TidyMaze <GrosKiff@gmail.com>
2016-02-01 08:33:04 +0100

    DLL de réservation : mocks

---
+++ libBookFlights/libBookFlights.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace booking.libBookFlights
+{
+    public class libBookFlights
+    {
+        /// <summary>
+        /// Ajoute une réservation d'un vol
+        /// </summary>
+        /// <param name="idClient">Identifiant commun du client</param>
+        /// <param name="idFlight">Identifiant du vol</param>
+        public void bookFlight(int idClient, int idFlight)
+        {
+
+        }
+    }
+}
---
+++ libBookHotels/libBookHotels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace booking.libBookHotels
+{
+    public class libBookHotels
+    {
+        /// <summary>
+        /// Ajoute une réservation d'un hotel
+        /// </summary>
+        /// <param name="idClient">identifiant commun du client</param>
+        /// <param name="idHotel">identifiant de l'hotel</param>
+        /// <param name="dateFrom">date de début de la réservation (limitée au jour)</param>
+        /// <param name="dateTo">date de fin de la réservation (limitée au jour)</param>
+        public void bookHotel(int idClient, int idHotel, DateTime dateFrom, DateTime dateTo)
+        {
+
+        }
+    }
+}
