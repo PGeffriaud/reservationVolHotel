c6d0f5cec14ee60f2ebd3eacb9869867367dcf53
TidyMaze <GrosKiff@gmail.com>
2016-02-01 12:04:42 +0100

    DLL de réservations : OK pour utilisation

--- libReservation/libReservation.cs
+++ libReservation/libReservation.cs
@@ -4,6 +4,8 @@ using System.EnterpriseServices;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using booking.libBookFlights;
+using booking.libBookHotels;
 
 namespace booking.libReservation
 {
@@ -13,7 +15,11 @@ namespace booking.libReservation
         [AutoComplete]
         public void reservation(int idClient, int idFlight, int idHotel, DateTime dateFrom, DateTime dateTo)
         {
-            
+            var libBookFlights = new libBookFlights.libBookFlights();
+            var libBookHotels = new libBookHotels.libBookHotels();
+
+            libBookFlights.bookFlight(idClient,idFlight);
+            libBookHotels.bookHotel(idClient,idHotel,dateFrom,dateTo);
         }
     }
 }
