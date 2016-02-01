4cfce3473c540474eea99077917b262728fa0b22
TidyMaze <GrosKiff@gmail.com>
2016-02-01 10:59:36 +0100

    DLL réservations DAL

--- libBookHotels/Properties/AssemblyInfo.cs
+++ libBookHotels/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.EnterpriseServices;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -17,7 +18,9 @@ using System.Runtime.InteropServices;
 // L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
 // aux composants COM.  Si vous devez accéder à un type dans cet assembly à partir de 
 // COM, affectez la valeur true à l'attribut ComVisible sur ce type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
+[assembly: Description("Booking EMN")]
+[assembly: ApplicationActivation(ActivationOption.Server)]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
 [assembly: Guid("8cd2715c-fbd0-4590-81ef-f5220def98c7")]
--- libBookHotels/libBookHotels.cs
+++ libBookHotels/libBookHotels.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.EnterpriseServices;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using libBookHotels.Properties;
 
 namespace booking.libBookHotels
 {
+    [Transaction(TransactionOption.Required), ObjectPooling(5, 10), Description("Booking EMN")]
     public class libBookHotels
     {
         /// <summary>
@@ -15,9 +19,21 @@ namespace booking.libBookHotels
         /// <param name="idHotel">identifiant de l'hotel</param>
         /// <param name="dateFrom">date de début de la réservation (limitée au jour)</param>
         /// <param name="dateTo">date de fin de la réservation (limitée au jour)</param>
+        [AutoComplete]
         public void bookHotel(int idClient, int idHotel, DateTime dateFrom, DateTime dateTo)
         {
-
+            SqlConnection connection = new SqlConnection();
+            var host = Settings.Default["host"];
+            connection.ConnectionString = "Data Source=" + host + "\\SQLEXPRESS;Initial Catalog=AIRFLIGHT;Integrated Security=True";
+            connection.Open();
+            SqlCommand myCmd = new SqlCommand("createBookHotel", connection);
+            myCmd.CommandType = System.Data.CommandType.StoredProcedure;
+            myCmd.Parameters.Add(new SqlParameter("@idClient", idClient));
+            myCmd.Parameters.Add(new SqlParameter("@idHotel", idHotel));
+            myCmd.Parameters.Add(new SqlParameter("@dateFrom", dateFrom));
+            myCmd.Parameters.Add(new SqlParameter("@dateTo", dateTo));
+            Convert.ToInt32(myCmd.ExecuteNonQuery());
+            connection.Close();
         }
     }
 }
